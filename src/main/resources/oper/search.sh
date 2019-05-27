31a5fa522d68c541aedee049a669af83c076a98c
cauchy8389 <cauchy8389@sina.com>
2019-05-27 09:42:46 +0800

    may be can use

--- src/main/resources/oper/search.sh
+++ src/main/resources/oper/search.sh
@@ -8,3 +8,13 @@ curl -H "Content-Type: application/json;charset=UTF-8" -XGET http://zhy.cauchy83
     }
   }
 }'
+
+curl -H "Content-Type: application/json;charset=UTF-8" -XGET http://zhy.cauchy8389.com:9200/esh_complaints/_search?pretty -d '
+{
+  "query" : {
+    "multi_match": {
+            "query" : "11935",
+            "fields" : ["complaintId", "zip"]
+     }
+  }
+}'
